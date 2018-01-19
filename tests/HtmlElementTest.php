229f6ec5a2b78be44e5f47b2b6a5aca89cdb76be
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-19 14:39:55 -0500

    Finished 5th lesson: automating the tests

--- tests/HtmlElementTest.php
+++ tests/HtmlElementTest.php
@@ -2,10 +2,77 @@
 
 namespace Tests;
 
+use App\HtmlElement;
+
 class HtmlElementTest extends TestCase
 {
-    public function test_it_generates_html_elements()
+    /** @test */
+    function it_generates_a_paragraph_with_content()
     {
-        $this->markTestIncomplete();
+        $element = new HtmlElement('p', [],'Este es el contenido');
+
+        $this->assertSame('<p>Este es el contenido</p>', $element->render());
+    }
+
+    /** @test */
+    function it_generates_a_paragraph_with_content_and_an_id_attribute()
+    {
+        $element = new HtmlElement('p', ['id' => 'my_paragraph'],'Este es el contenido');
+
+        $this->assertSame('<p id="my_paragraph">Este es el contenido</p>', $element->render());
+    }
+
+    /** @test */
+    function it_generates_a_paragraph_with_multiple_attributes()
+    {
+        $element = new HtmlElement('p', ['id' => 'my_paragraph', 'class' => 'paragraph'],'Este es el contenido');
+
+        $this->assertSame(
+            '<p id="my_paragraph" class="paragraph">Este es el contenido</p>',
+            $element->render()
+        );
+    }
+
+    /** @test */
+    function it_generates_an_img_tag()
+    {
+        $element = new HtmlElement('img', ['src' => 'img/styde.png']);
+
+        $this->assertSame('<img src="img/styde.png">', $element->render());
+    }
+
+    /** @test */
+    function it_escapes_the_html_attributes()
+    {
+        $element = new HtmlElement('img', ['src' => 'img/styde.png', 'title' => 'Curso de "Refactorización" en Styde']);
+
+        $this->assertSame(
+            '<img src="img/styde.png" title="Curso de &quot;Refactorizaci&oacute;n&quot; en Styde">',
+            $element->render()
+        );
+    }
+
+    /** @test */
+    function it_generates_elements_with_boolean_attributes()
+    {
+        $element = new HtmlElement('input', ['required']);
+
+        $this->assertSame('<input required>', $element->render());
+    }
+
+    /** @test */
+    function it_generates_elements_with_mixed_attributes()
+    {
+        $element = new HtmlElement('input', ['type'=>'text', 'required']);
+
+        $this->assertSame('<input type="text" required>', $element->render());
+    }
+
+    /** @test */
+    function it_ignores_content_with_void_elements()
+    {
+        $element = new HtmlElement('input', ['required'], 'Aquí no debería haber contenido');
+
+        $this->assertSame('<input required>', $element->render());
     }
 }
