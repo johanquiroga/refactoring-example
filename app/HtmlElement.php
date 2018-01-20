df069ced8476ee98c2439b3d4b874def6ccc5477
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-20 17:53:10 -0500

    Finished 8th lesson: calisthenics of objects, don't use 'else' (Guard Clauses)

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -43,13 +43,7 @@ class HtmlElement
 
     public function open()
     {
-        if (! empty($this->attributes)) {
-            $result = "<{$this->name}{$this->attributes()}>";
-        } else {
-            $result = "<{$this->name}>";
-        }
-
-        return $result;
+        return "<{$this->name}{$this->attributes()}>";
     }
 
     public function attributes()
@@ -63,17 +57,18 @@ class HtmlElement
         return $htmlAttributes;
     }
 
+    public function hasAttributes()
+    {
+        return ! empty($this->attributes);
+    }
+
     protected function renderAttribute($attribute, $value)
     {
-        $htmlAttribute = '';
-
         if (is_numeric($attribute)) {
-            $htmlAttribute .= ' '.$value;
-        } else {
-            $htmlAttribute .= ' '.$attribute.'="'.htmlentities($value, ENT_QUOTES, 'UTF-8').'"'; // name="value"
+            return ' '.$value;
         }
 
-        return $htmlAttribute;
+        return ' '.$attribute.'="'.htmlentities($value, ENT_QUOTES, 'UTF-8').'"'; // name="value"
     }
 
     public function isVoid()
